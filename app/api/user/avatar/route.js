ee7f45ec874739833655fd619edb6b84f65023f1
Karenipo1 <perezortuno.ki@gmail.com>
2025-12-08 16:09:43 +0200

    fix connect import

--- app/api/user/avatar/route.js
+++ app/api/user/avatar/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import User from "@/models/User";
-import connectDB from "@/lib/mongodb";
+import {connectDB} from "@/lib/mongodb";
 import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 
 export async function PATCH(req) {
